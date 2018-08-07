054a06f99cb6285aeb8055f8a767f4fa41190b48
rbhansali9 <rbhansali9@gatech.edu>
2018-08-07 12:51:10 -0400

    implemented StudentControllerServlet.java

---
+++ UniversitySystem/src/com/luv2code/web/jdbc/StudentControllerServlet.java
@@ -0,0 +1,69 @@
+package com.luv2code.web.jdbc;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ * Servlet implementation class StudentControllerServlet
+ */
+@WebServlet("/StudentControllerServlet")
+public class StudentControllerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private StudentDbUtil studentDbUtil;
+	
+	@Resource(name="jdbc/web_student_tracker")
+	private DataSource dataSource;
+
+	
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+		
+		//crete studentDbutil and pass datasource
+		try {
+			studentDbUtil = new StudentDbUtil(dataSource);
+		}
+		catch(Exception exc){
+			throw new ServletException(exc);
+		}
+	}
+
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			listStudent(request,response);
+		}
+		catch(Exception ex) {
+			throw new ServletException(ex);
+		}
+		
+	}
+
+
+	private void listStudent(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		//get the students from db util
+		List<Student> students = studentDbUtil.getStudents();
+		//add students to the request
+		request.setAttribute("STUDENT_LIST", students);
+		
+		//send to JSP page (View)
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/list-student.jsp");
+		dispatcher.forward(request, response);
+		
+	}
+
+}
